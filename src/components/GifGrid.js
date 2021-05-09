d04065ec40319abd919185d4d68ec356b39576a4
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-05-09 00:22:38 -0500

    GifItem Element.

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,38 +1,44 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import GifGridItem from "./GifGridItem";
 
-const GifGrid = ({category}) => {
+const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
 
-  const search = 'naruto';
-  const apiKey = 'qNl9pBFbmmnY9rKjK8p30Zh5aCMqJP3C';
-  const limit = 10
+  const search = "naruto";
+  const apiKey = "qNl9pBFbmmnY9rKjK8p30Zh5aCMqJP3C";
+  const limit = 10;
 
-  const getGifs = async() => {
+  useEffect(() => {
+    getGifs();
+  }, []);
+
+  const getGifs = async () => {
     const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}&limit=${limit}`;
     const resp = await fetch(url);
-    const {data} = await resp.json();
-    const gifs = data.map( img => {
+    const { data } = await resp.json();
+    const gifs = data.map((img) => {
       return {
         id: img.id,
         title: img.title,
-        url: img.images.downsized_medium.url
-      }
+        url: img.images.downsized_medium.url,
+      };
     });
 
-    console.log(gifs);
-  }
-
-  getGifs();
+    setImages(gifs);
+  };
 
   return (
     <div>
-      <h2>{category}</h2>
+      <ol className="card-grid">
+        {images.map((img) => (
+          <GifGridItem {...img} />
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-GifGrid.propTypes = {
+GifGrid.propTypes = {};
 
-}
-
-export default GifGrid
+export default GifGrid;
---
+++ src/components/GifGridItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GifGridItem = ({id, title, url}) => {
+  return (
+    <div className="card" >
+      <img src={url} alt={title} />
+      <p>{title}</p>
+    </div>
+  )
+}
+
+GifGridItem.propTypes = {
+
+}
+
+export default GifGridItem
